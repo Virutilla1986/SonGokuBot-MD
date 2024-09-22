ce7674a79afc0aa69bcaee5cb4d107916628fbaa
MundoDragonBall <teresacortesoliva1960@gmail.com>
2024-09-22 20:46:05 +0200

    Add files via upload

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Juan1986;;\nFN:juann \nORG:juan1986 🌟\nTITLE:\nitem1.TEL;waid=34636445091:34636445091\nitem1.X-ABLabel:Juan1986 🌟\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Juan1986  🌟\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'Juan1986.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:YerTX2;;\nFN:YerTX2 \nORG:YerTX2 🌟\nTITLE:\nitem1.TEL;waid=51907376960:51907376960\nitem1.X-ABLabel:YerTX2 🌟\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:YerTX2  🌟\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: 'YerTX2.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
 handler.tags = ['main']
