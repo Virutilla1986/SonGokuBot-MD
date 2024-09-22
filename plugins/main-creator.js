68268fdf349f2ed56678914311c373885616bce9
MundoDragonBall <teresacortesoliva1960@gmail.com>
2024-09-22 20:43:58 +0200

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Juan1986;;\nFN:YerTX2 \nORG:juan1986 🌟\nTITLE:\nitem1.TEL;waid=34636445091:34636445091\nitem1.X-ABLabel:Juan1986 🌟\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Juan1986  🌟\nEND:VCARD`
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Juan1986;;\nFN:juann \nORG:juan1986 🌟\nTITLE:\nitem1.TEL;waid=34636445091:34636445091\nitem1.X-ABLabel:Juan1986 🌟\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Juan1986  🌟\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: 'Juan1986.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
