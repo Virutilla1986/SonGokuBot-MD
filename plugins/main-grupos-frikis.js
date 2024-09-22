8dd85930e4aad2e9a9dbe41d3c091cd46a5dc14c
MundoDragonBall <teresacortesoliva1960@gmail.com>
2024-09-22 22:54:23 +0200

    Add files via upload

---
+++ plugins/main-grupos-frikis.js
@@ -0,0 +1,79 @@
+import fetch from 'node-fetch'
+
+let handler  = async (m, { conn, usedPrefix, command }) => {
+let img = await (await fetch(`https://telegra.ph/file/0dbf7b27644e8dfa8baa4.jpg`)).buffer()
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+let txt = `*Hola!, te invito a formar parte de nuestros grupos de dragón ball.* 
+
+*◜🐉 GRUPOS OFICIALES 🐉◞*
+
+${DragonBallLegends}
+${gp1}
+
+${FamiliaSaiyan}
+${gp2}
+
+${DragonBallFornite}
+${gp3}
+
+${DragonBallRol}
+${gp4}
+
+${DragonBallAF}
+${gp5}
+
+${VillanoDragonBall}
+${gp6}
+
+${DragonBallXenoverse}
+${gp7}
+
+${VideosDragonBall}
+${gp8}
+
+${DragonBallStickers}
+${gp9}
+
+${DragonBallDibujos}
+${gp10}
+
+${PowerOfGods}
+${gp11}
+
+${DragonBallEncuestas}
+${gp12}
+
+${DragonBallKakarot}
+${gp13}
+
+${DragonBallZDokkanBattle}
+${gp14}
+
+${DragonBallZFans}
+${gp15}
+
+${FansDragonBallZKai}
+${gp16}
+
+*◆━━━━━━━▣✦▣━━━━━━━━◆*
+
+*◜💦 CANALES OFICIALES 💦◞* 
+
+${Noticiasdragonball}
+${canal} 
+
+${Curiosidaddragonball}
+${canal2} 
+
+${Dragonballjuegos}
+${canal3} 
+
+
+> 🚩 ${textbot}`
+await conn.sendFile(m.chat, img, "Thumbnail.jpg", txt, m, null, rcanal)
+}
+handler.help = ['grupos']
+handler.tags = ['main']
+handler.command = /^(grupos)$/i
+export default handler
